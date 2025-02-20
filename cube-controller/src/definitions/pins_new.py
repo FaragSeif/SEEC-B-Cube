bed7b2c93f8d89d2aa5d45e83e374852c7af6d69
Seif Farag <seif.abdelrhman60@gmail.com>
2025-02-20 22:38:34 +0900

    Initial commit

---
+++ cube-controller/src/definitions/pins_new.py
@@ -0,0 +1,21 @@
+#ifndef pins_h
+#define pins_h
+
+// Motor controllers
+const int M1_ENABLE = 13;
+const int M1_CURRENT = 12;
+const int M2_ENABLE = 17;
+const int M2_CURRENT = 18;
+const int M3_ENABLE = 5;
+const int M3_CURRENT = 6;
+
+// Motor speeds
+const int M1_SPEED = 10;
+const int M2_SPEED = 9;
+const int M3_SPEED = 7;
+
+// IMU
+const int IMU_SDA = 47;
+const int IMU_SCL = 21;
+
+#endif
